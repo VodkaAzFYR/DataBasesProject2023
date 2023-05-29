f2a6f254277cf5592ee3a248800bc4a97884330c
JakubMarzeda <jakub.marzeda11@gmail.com>
2023-05-29 21:58:33 +0200

    Create tables

--- CreatDabaseAndTables.sql
+++ CreatDabaseAndTables.sql
@@ -9,6 +9,22 @@ GO
 
 USE [LessonPlan]
 GO
+CREATE TABLE Class(
+	Id INT IDENTITY(1, 1),
+	TeacherId INT NOT NULL,
+	ClassroomId INT NOT NULL,
+	ClassName NVARCHAR(50) NOT NULL,
+	BirthYear DATE NOT NULL,
+	CONSTRAINT PK_Class_Id PRIMARY KEY (Id)
+)
+CREATE TABLE Teacher(
+	Id INT IDENTITY(1, 1),
+	ClassId INT NOT NULL,
+	TeacherName NVARCHAR(50) NOT NULL,
+	TeacherLastname NVARCHAR(50) NOT NULL,
+	TeacherPhoneNumber NVARCHAR(9) CHECK(LEN(TeacherPhoneNumber) = 9) NULL,
+	CONSTRAINT PK_Teacher_Id PRIMARY KEY (Id)
+)
 
 CREATE TABLE [Subject](
 	Id INT IDENTITY(1, 1),
@@ -16,25 +32,30 @@ CREATE TABLE [Subject](
 	SubjectDescription NVARCHAR(255) NULL,
 	CONSTRAINT PK_Subject_Id PRIMARY KEY (Id)
 )
-CREATE TABLE Teacher(
-	Id INT IDENTITY(1, 1),
-	TeacherName NVARCHAR(50) NOT NULL,
-	TeacherLastname NVARCHAR(50) NOT NULL,
-	TeacherPhoneNumber NVARCHAR(9) CHECK(LEN(TeacherPhoneNumber) = 9) NULL,
-	CONSTRAINT PK_Teacher_Id PRIMARY KEY (Id)
-)
-CREATE TABLE Class(
-	Id INT IDENTITY(1, 1),
-	ClassName NVARCHAR(50) NOT NULL,
-	BirthYear DATE NOT NULL,
-	CONSTRAINT PK_Class_Id PRIMARY KEY (Id)
-)
 CREATE TABLE Classroom(
 	Id INT IDENTITY(1, 1),
-	TeacherId INT NOT NULL,
 	ClassId INT NOT NULL,
 	ClassroomNumber INT NOT NULL,
 	ClassroomQuantity INT NULL,
 	CONSTRAINT PK_Classroom_Id PRIMARY KEY (Id)
 )
+CREATE TABLE SubjectTeacher(
+	Id INT IDENTITY(1, 1),
+	SubjectId INT NOT NULL,
+	TeacherId INT NOT NULL,
+	CONSTRAINT PK_SubjectTeacher_Id PRIMARY KEY (Id),
+	CONSTRAINT FK_SubjectTeacher_SubjectId FOREIGN KEY (SubjectId) REFERENCES [Subject](Id),
+	CONSTRAINT FK_SubjectTeacher_TeacherId FOREIGN KEY (TeacherId) REFERENCES Teacher(Id)
+)
 
+ALTER TABLE Class
+ADD CONSTRAINT FK_Class_TeacherId FOREIGN KEY (TeacherId) REFERENCES Teacher(Id)
+
+ALTER TABLE Class
+ADD CONSTRAINT FK_Class_ClassroomId FOREIGN KEY (ClassroomId) REFERENCES Classroom(Id)
+
+ALTER TABLE Teacher
+ADD CONSTRAINT FK_Teacher_ClassId FOREIGN KEY (ClassId) REFERENCES Class(Id)
+
+ALTER TABLE Classroom
+ADD CONSTRAINT FK_Classroom_ClassId FOREIGN KEY (ClassId) REFERENCES Class(Id)
