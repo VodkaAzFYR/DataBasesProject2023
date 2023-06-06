8e2c2ca5e695149b9c2fe0a86c3ee378666e923a
VodkaAzFYR <antossmaga2007@gmail.com>
2023-06-06 10:19:57 +0200

    Add get lessons plan endpoint and select lessons data function

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 from fastapi import FastAPI
 import os
-import pyodbc
+from database import DataBase
+from datetime import datetime
 
-connection_string = ("Driver={ODBC Driver 17 for SQL Server};"
-                    "Server= localhost;"
-                    "Database=LessonPlan;"
-                    "Trusted_Connection=yes;")
-cnxn = pyodbc.connect(connection_string)
-cursor = cnxn.cursor()
-cursor.execute("SELECT * FROM Teacher")
-row = cursor.fetchall()
-print(row)
 app = FastAPI()
 @app.get("/")
 def root():
-    return {"Hey"}
+    return {"status": "ready"}
 
+
+@app.get("/get_lessons_plan")
+def get_lessons_plan(date, class_name):
+    datetime_date = datetime.strptime(date, f"%d-%m-%Y")
+    db = DataBase()
+    data = db.select_lessons_data(date=datetime_date, class_name=class_name)
+    return f"{data}"
 if __name__ == "__main__":
     os.system("python -m uvicorn main:app --reload")  #--host 0.0.0.0
